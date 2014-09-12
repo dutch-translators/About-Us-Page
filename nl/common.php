7a024dcd00e1dff6d92f40667dfd098ae78a563d
David <neurofibromatose@live.nl>
2014-09-12 19:49:01 +0200

    eerste vertaling

--- nl/common.php
+++ nl/common.php
@@ -1,9 +1,10 @@
 <?php
 /**
 *
-* @package phpBB Extension - Crizzo About Us (English)
+* @package phpBB Extension - Crizzo About Us (Dutch)
 * @copyright (c) 2014 phpBB Group
 * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 *
 */
 
@@ -18,16 +19,16 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ABOUTUS'						=> 'About Us',
-	'ABOUTUS_SETTINGS'				=> 'About Us settings',
-	'ABOUTUS_UPDATED'				=> 'About Us was successfully updated.',
+	'ABOUTUS'						=> 'Over Ons',
+	'ABOUTUS_SETTINGS'				=> 'Over ons Instellingen',
+	'ABOUTUS_UPDATED'				=> 'Over ons is succesvol bijgewerkt.',
 
-	'ACP_ABOUTUS_ENABLE'			=> 'About Us',
-	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> '"Enabled" displays the link in the footer and the page to the user. "Disabled" does the opposite but will not delete your About Us text.',
-	'ACP_ABOUTUS_SETTINGS'			=> 'About Us settings',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Here you are able to define the text of the "About Us" page and enable or disable the "About us"-page.',
-	'ACP_ABOUTUS_INFO'				=> 'About Us text',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'You can edit the text which is displayed on the About Us page.',
+	'ACP_ABOUTUS_ENABLE'			=> 'Over Ons',
+	'ACP_ABOUTUS_ENABLE_EXPLAIN'	=> 'Wanneer "ingeschakeld" wordt er een link in de footer van de pagina weergeven die linkt naar de "Over ons" pagina. Wanneer "Uitgeschakeld" wordt de link in de footer niet getoond, deze optie zal de tekst die je hebt opgemaakt niet verwijderen.',
+	'ACP_ABOUTUS_SETTINGS'			=> 'Over ons instellingen',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de "Over ons" pagina aanpassen en je kan de "Over ons"-pagina inschakelen of uitschakelen.',
+	'ACP_ABOUTUS_INFO'				=> 'Over ons Tekst',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de "Over ons" pagina.',
 
-	'VIEWONLINE_ABOUTUS'	=> 'Viewing About Us',
+	'VIEWONLINE_ABOUTUS'	=> 'Bekijkt Over ons',
 ));
