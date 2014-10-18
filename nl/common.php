cb7afb7c092ea09a669f934fe6abbf5dcca365b9
David <neurofibromatose@live.nl>
2014-10-18 22:19:02 +0200

    Update common.php
    Crizzo had opgemerkt dat ik/wij 'over ons pagina' verschillende keren op een andere manier hadden geschreven. Hij vroeg zich af wat nu de juiste manier is:
    
    "Over ons" pagina
    "Over ons"-pagina
    over ons pagina
    
    Ik heb gekozen voor de laatste optie, mocht één van de andere opties beter zijn of een combinatie daarvan, dan hoor ik het graag.

--- nl/common.php
+++ nl/common.php
@@ -20,14 +20,14 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ABOUTUS'						=> 'Over Ons',
-	'ABOUTUS_SETTINGS'				=> 'Over ons Instellingen',
+	'ABOUTUS_SETTINGS'				=> 'Over ons instellingen',
 	'ABOUTUS_UPDATED'				=> 'Over ons is succesvol bijgewerkt.',
 
-	'ACP_ABOUTUS_INFO'				=> 'Over ons Tekst',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de "Over ons" pagina.',	
+	'ACP_ABOUTUS_INFO'				=> 'Over ons tekst',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de over ons pagina.',	
     'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Over ons voorbeeld',	
 	'ACP_ABOUTUS_SETTINGS'			=> 'Over ons instellingen',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de "Over ons" pagina aanpassen en je kan de "Over ons"-pagina inschakelen of uitschakelen.',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de over ons pagina aanpassen en je kan de over ons pagina inschakelen of uitschakelen.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Laat voorwaarden voor gebruik zien',
 	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Laat een link naar de voorwaarden voor gebruik zien op de over ons pagina.',
