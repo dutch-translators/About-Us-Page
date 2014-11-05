7043aade982670228e1551b5332598487d057c23
Rikkert Westra <rotsblok@phpbbservice.nl>
2014-11-05 16:41:41 +0100

    aanpassing

--- nl/common.php
+++ nl/common.php
@@ -20,19 +20,19 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ABOUTUS'						=> 'Over Ons',
-	'ABOUTUS_SETTINGS'				=> 'Over ons instellingen',
-	'ABOUTUS_UPDATED'				=> 'Over ons is succesvol bijgewerkt.',
+	'ABOUTUS_SETTINGS'				=> '“Over ons”-instellingen',
+	'ABOUTUS_UPDATED'				=> '“Over ons” is succesvol bijgewerkt.',
 
-	'ACP_ABOUTUS_INFO'				=> 'Over ons tekst',
-	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de over ons pagina.',	
-    'ACP_ABOUTUS_INFO_PREVIEW'		=> 'Over ons voorbeeld',	
-	'ACP_ABOUTUS_SETTINGS'			=> 'Over ons instellingen',
-	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de over ons pagina aanpassen en je kan de over ons pagina inschakelen of uitschakelen.',
+	'ACP_ABOUTUS_INFO'				=> '“Over ons”-tekst',
+	'ACP_ABOUTUS_INFO_EXPLAIN'		=> 'Hier kan je de tekst aanpassen die wordt weergegeven op de “Over ons”-pagina.',	
+    'ACP_ABOUTUS_INFO_PREVIEW'		=> '“Over ons” voorbeeld',	
+	'ACP_ABOUTUS_SETTINGS'			=> '“Over ons”-instellingen',
+	'ACP_ABOUTUS_SETTINGS_EXPLAIN'	=> 'Hier kan je de tekst voor de “Over ons”-pagina aanpassen en je kan de “Over ons”-pagina inschakelen of uitschakelen.',
 
 	'ACP_TERMS_OF_USE_ENABLE' 			=> 'Laat voorwaarden voor gebruik zien',
-	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Laat een link naar de voorwaarden voor gebruik zien op de over ons pagina.',
+	'ACP_TERMS_OF_USE_ENABLE_EXPLAIN'	=> 'Laat een link naar de voorwaarden voor gebruik zien op de “Over ons”-pagina.',
 	'ACP_PRIVACY_ENABLE' 				=> 'Laat privacybeleid zien',
-	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Laat een link naar het privacybeleid zien op de over ons pagina.',
+	'ACP_PRIVACY_ENABLE_EXPLAIN'		=> 'Laat een link naar het privacybeleid zien op de “Over ons”-pagina.',
 
 	'ABOUT_US_AND'					=> 'en',
 
@@ -40,5 +40,5 @@ $lang = array_merge($lang, array(
 	'DESCRIPTION_TERMS_OF_USE'		=> 'Je kan de voorwaarden voor gebruik hier zien:',
 	'DESCRIPTION_PRIVACY_TERMS_OF_USE'=> 'Je kan de voorwaarden voor gebruik en het privacybeleid hier zien:',	
 	
-	'VIEWONLINE_ABOUTUS'	=> 'Bekijkt Over ons',
+	'VIEWONLINE_ABOUTUS'	=> 'Bekijkt “Over ons”',
 ));
